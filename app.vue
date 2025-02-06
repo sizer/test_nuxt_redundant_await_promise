582979dc7833e0be7137b96272a8ab2f3d359fbf
sizer <shinse.tanaka@gmail.com>
2025-02-06 23:52:47 +0900

    add some debug cases

--- app.vue
+++ app.vue
@@ -1,15 +1,24 @@
 <template>
   <div style="display: flex; flex-direction: column; gap: 1rem; width: 50rem; margin: 0 auto;">
-    <h1>My Status: {{ mystatusStore.status }}</h1>
+    <h1>
+      mystatusStore.status: {{ mystatusStore.status }} <br>
+      storeToRefs(mystatusStore).status: {{ mystatusRef }}
+    </h1>
     <button @click="mystatusStore.setStatus('default')">Set Status to default</button>
 
+    <h2>Failed: never display "started"...</h2>
     <button @click="handleClickMeaningLessLongCalcTimeLogic">meaninglessLongCalcTimeLogic</button>
+    <button @click="handleClickMeaningLessLongCalcTimeButAsyncLogic">meaninglessLongCalcTime - ButAsyncLogic</button>
+    <button @click="handleClickMeaningLessLongCalcTimeLogicWithNextTick">meaninglessLongCalcTimeLogic -
+      await nextTick();</button>
+    <button @click="handleClickMeaningLessLongCalcTimeLogicWithRequestAnimationFrame">meaninglessLongCalcTimeLogic -
+      requestAnimationFrame</button>
 
-    <button @click="handleClickMeaningLessLongCalcTimeLogicWithSetTimeoutZero">meaninglessLongCalcTimeLogic with await
+    <h2>Success: display "started"</h2>
+    <button @click="handleClickMeaningLessLongCalcTimeLogicWithSetTimeoutZero">meaninglessLongCalcTimeLogic - with await
       new
       Promise((resolve) => setTimeout(resolve, 0))</button>
 
-    <button @click="handleClickMeaningLessLongCalcTimeButAsyncLogic">meaninglessLongCalcTimeButAsyncLogic</button>
   </div>
 </template>
 
@@ -17,6 +26,9 @@
 import { useMystatusStore } from '~/stores/mystatus';
 
 const mystatusStore = useMystatusStore();
+const {
+  status: mystatusRef
+} = storeToRefs(mystatusStore);
 
 const meaninglessLongCalcTimeLogic = (): void => {
   const start = Date.now();
@@ -38,17 +50,6 @@ const handleClickMeaningLessLongCalcTimeLogic = () => {
   mystatusStore.setStatus("completed");
 };
 
-const handleClickMeaningLessLongCalcTimeLogicWithSetTimeoutZero = async () => {
-  mystatusStore.setStatus("started");
-
-  // display "started" when thie line called
-  await new Promise((resolve) => setTimeout(resolve, 0));
-
-  meaninglessLongCalcTimeLogic();
-
-  mystatusStore.setStatus("completed");
-};
-
 const handleClickMeaningLessLongCalcTimeButAsyncLogic = async () => {
   mystatusStore.setStatus("started");
 
@@ -61,4 +62,33 @@ const handleClickMeaningLessLongCalcTimeButAsyncLogic = async () => {
   mystatusStore.setStatus("completed");
 };
 
+const handleClickMeaningLessLongCalcTimeLogicWithNextTick = async () => {
+  mystatusStore.setStatus("started");
+
+  // never display "started"...
+  await nextTick();
+  meaninglessLongCalcTimeLogic();
+
+  mystatusStore.setStatus("completed");
+};
+
+const handleClickMeaningLessLongCalcTimeLogicWithRequestAnimationFrame = async () => {
+  mystatusStore.setStatus("started");
+
+  // never display "started"...
+  requestAnimationFrame(meaninglessLongCalcTimeLogic);
+
+  mystatusStore.setStatus("completed");
+};
+
+const handleClickMeaningLessLongCalcTimeLogicWithSetTimeoutZero = async () => {
+  mystatusStore.setStatus("started");
+
+  // display "started" when thie line called
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  meaninglessLongCalcTimeLogic();
+
+  mystatusStore.setStatus("completed");
+};
 </script>
